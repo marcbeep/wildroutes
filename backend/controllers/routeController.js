f8a2c51a32c2ecd25d841575c791ccbd14f2c4c1
marcbeep <77645923+marcbeep@users.noreply.github.com>
2023-03-30 10:03:49 +0100

    Inital controller setup

---
+++ backend/controllers/routeController.js
@@ -0,0 +1,53 @@
+// Import model
+const Route = require('../models/routemodel')
+
+// Import mongoose
+const mongoose = require('mongoose')
+
+// Get all routes
+const getRoutes = async(req, res) => {
+    const routes = await Route.find({})
+    res.status(200).json(routes)
+}
+
+// Get single route
+const getRoute = async(req, res) => {
+    const {id} = req.params
+
+    // Checks to see if id is valid
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'No such route'})
+    }
+
+    const route = await Route.findById(id)
+
+    if(!route){
+        return res.status(404).json({error: 'No such route'})
+    }
+
+    res.status(200).json(route)
+}
+
+// Create new route
+const createRoute = async(req, res) => {
+    const{title, description, location} = req.body
+    
+    // Add doc to db
+    try{
+        const route = await Route.create({title, description, location})
+        res.status(200).json(route)
+    }catch(error){
+        res.status(400).json({error: error.message})
+    }
+}
+
+// Delete single route
+
+// Update single route
+
+// Exports
+module.exports = {
+    getRoutes,
+    getRoute,
+    createRoute
+}
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,33 +1,22 @@
+// Imports
+const {
+    getRoutes,
+    getRoute,
+    createRoute
+}= require('../controllers/routeController')
+
 // We create the express router
 const express = require('express')
 const router = express.Router()
 
-// Import model
-const Route = require('../models/routemodel')
-
 // Get all routes
-router.get('/', (req, res) => {
-    res.json({mssg: "GET all routes"})
-})
+router.get('/', getRoutes)
 
 // Get a single route
-router.get('/:id', (req, res) => {
-    res.json({mssg: "GET a single route"})
-})
+router.get('/:id', getRoute)
 
 // Post a new route
-router.post('/', async (req, res) => {
-    const{title, description, location} = req.body
-    
-    try{
-        const route = await Route.create({title, description, location})
-        res.status(200).json(route)
-    }catch(error){
-        res.status(400).json({error: error.message})
-    }
-
-    res.json({mssg: "POST a new route"})
-})
+router.post('/', createRoute)
 
 // Delete a route
 router.delete('/:id', (req, res) => {
