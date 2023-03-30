bdd4478cfd0b07d272e2b459879b8da5ecbbaabd
marcbeep <77645923+marcbeep@users.noreply.github.com>
2023-03-30 10:46:01 +0100

    Initial React app setup

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+// Ensure npm install react-router-dom
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+
+// Pages and Componenets
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={<Home />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
