e0d2687d91e35ba80342fd5a799a240951439e80
marcbeep <77645923+marcbeep@users.noreply.github.com>
2023-04-04 16:43:58 +0100

    Added Auth Context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { RoutesContextProvider } from './context/RouteContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RoutesContextProvider>
-      <App />
-    </RoutesContextProvider>
+    <AuthContextProvider>
+      <RoutesContextProvider>
+        <App />
+      </RoutesContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
